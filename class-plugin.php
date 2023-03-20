1d58678514ee002fdd9d3f1ce50d8b24775d6211
Robert O'Rourke <robert@o-rourke.org>
2023-03-20 17:40:24 +0000

    Initial version

---
+++ class-plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace HM\MUPluginLoader;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface {
+
+	/**
+	 * The composer class.
+	 *
+	 * @var Composer
+	 */
+	protected $composer;
+
+	/**
+	 * Called when the plugin is activated.
+	 *
+	 * @param Composer $composer The composer class.
+	 */
+	public function activate( Composer $composer ) {
+		$this->composer = $composer;
+	}
+
+	public static function getSubscribedEvents() {
+		return array(
+			'post-autoload-dump' => 'installLoader',
+		);
+	}
+
+	public function installLoader() {
+		$extra = $this->composer->getPackage()->getExtra();
+		$hm_mu_plugins = $extra['mu-plugins'] ?? [];
+		$hm_mu_plugins_path = $extra['mu-plugins-path'] ?? 'wp-content/mu-plugins';
+
+		$loader = file_get_contents( __DIR__ . '/loader.php' );
+		$loader = str_replace( '$hm_mu_plugins = []', '$hm_mu_plugins = ' . var_export( $hm_mu_plugins, true ), $loader );
+
+		$dest = dirname( $this->composer->getConfig()->get( 'vendor-dir' ) )
+			. DIRECTORY_SEPARATOR . $hm_mu_plugins_path
+			. DIRECTORY_SEPARATOR . 'loader.php';
+		file_put_contents( $dest, $loader );
+	}
+
+}
---
+++ loader.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Plugin Name: HM MU Plugin Loader
+ * Description: Loads the MU plugins required to run the site
+ * Author: Human Made Limited
+ * Author URI: http://hmn.md/
+ * Version: 1.0
+ */
+
+if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) ) {
+	return;
+}
+
+$hm_mu_plugins = [];
+
+if ( defined( 'HM_MU_PLUGINS' ) && is_array( HM_MU_PLUGINS ) ) {
+    $hm_mu_plugins = array_merge( $hm_mu_plugins, HM_MU_PLUGINS );
+}
+
+$hm_mu_plugins = array_unique( $hm_mu_plugins );
+
+foreach ( $hm_mu_plugins as $file ) {
+	require_once WPMU_PLUGIN_DIR . '/' . $file;
+}
+
+unset( $file );
+
+add_action( 'pre_current_active_plugins', function() use ( $hm_mu_plugins ) {
+	global $plugins, $wp_list_table;
+
+	// Add our own mu-plugins to the page.
+	foreach ( $hm_mu_plugins as $plugin_file ) {
+		$plugin_data = get_plugin_data( WPMU_PLUGIN_DIR . "/$plugin_file", false, false ); // Do not apply markup/translate as it'll be cached.
+
+		if ( empty( $plugin_data['Name'] ) ) {
+			$plugin_data['Name'] = $plugin_file;
+		}
+
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+		$plugins['mustuse'][ $plugin_file ] = $plugin_data;
+	}
+
+	// Recount totals.
+	// phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+	$GLOBALS['totals']['mustuse'] = count( $plugins['mustuse'] );
+
+	// Only apply the rest if we're actually looking at the page.
+	if ( 'mustuse' !== $GLOBALS['status'] ) {
+		return;
+	}
+
+	// Reset the list table's data.
+	$wp_list_table->items = $plugins['mustuse'];
+	foreach ( $wp_list_table->items as $plugin_file => $plugin_data ) {
+		$wp_list_table->items[ $plugin_file ] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
+	}
+
+	$total_this_page = $GLOBALS['totals']['mustuse'];
+
+	if ( $GLOBALS['orderby'] ) {
+		uasort( $wp_list_table->items, [ $wp_list_table, '_order_callback' ] );
+	}
+
+	// Force showing all plugins.
+	// See https://core.trac.wordpress.org/ticket/27110.
+	$plugins_per_page = $total_this_page;
+
+	$wp_list_table->set_pagination_args( [
+		'total_items' => $total_this_page,
+		'per_page'    => $plugins_per_page,
+	] );
+} );
+
+add_action( 'network_admin_plugin_action_links', function( $actions, $plugin_file, $plugin_data, $context ) use ( $hm_mu_plugins ) {
+	if ( $context !== 'mustuse' || ! in_array( $plugin_file, $hm_mu_plugins, true ) ) {
+		return [];
+	}
+
+	$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
+
+	return $actions;
+}, 10, 4 );
